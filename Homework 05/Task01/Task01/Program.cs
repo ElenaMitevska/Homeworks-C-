a51b8a45325a24d2659385bf1abccc76e3f2925c
unknown <elena_mitevska5@yahoo.com>
2024-03-10 22:31:46 +0100

    l

---
+++ Homework 05/Task01/Task01/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a method called AgeCalculator
+ The method will have one input parameter, your birthday date
+ The method should return your age
+ Show the age of a user after he inputs a date
+ Note: take into consideration if the birthday is today, after or before today*/
+            
+            int birthYear;
+            while (true)
+            {
+                Console.WriteLine("Enter your Year of Birth");
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out birthYear))
+                {
+                    Console.WriteLine("Wrong input, try again");
+                    continue;
+              
+                }
+                break;
+            }
+            Console.WriteLine($"Your birthday year is: {birthYear}");
+            Console.WriteLine($"Your age is: {YourAge(birthYear)}");
+
+
+            Console.ReadLine();
+        }
+        public static int YourAge(int value)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            int currentYear = currentDateTime.Year;
+            int result = currentYear - value;
+            return result;
+        }
+    }
+}
---
+++ Homework 05/Task2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a new console application and a new method called Substrings
+
+ In that method enter the following string :
+ "Hello from SEDC Codecademy 2024"
+ Ask the user to enter a number n.
+ Print the first n characters from that string and print the length of the new string.
+ Try to handle all the scenarios.*/
+            string greeting = "Hello from SEDC Codeacademy 2024";
+            string result;
+            result = Substring(greeting);
+
+        }
+        public static string Substring(string value)
+        {
+
+            int n;
+            while (true)
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out n))
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+                break;
+            }
+          
+            string substring = value.Substring(0, n);
+            Console.WriteLine(substring);
+            Console.WriteLine(substring.Length);
+            return substring;
+
+        }
+    }
+}
