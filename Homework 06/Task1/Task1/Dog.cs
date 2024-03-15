6cbcedb5cdbba585283da4f77a62e39ea56be668
unknown <elena_mitevska5@yahoo.com>
2024-03-15 19:37:52 +0100

    l

---
+++ Homework 06/Task1/Task1/Dog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Eat()
+        {
+            return $"The dog is now eating!";
+        }
+        public string Play()
+        {
+            return $"A dog is now playing!";
+        }
+        public string ChaseTail()
+        {
+            return $"A dog is now chaising it's tail.";
+        }
+        public Dog()
+        {
+
+        }
+        public Dog(string name, string breed, string color)
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+    
+    }
+}
+
---
+++ Homework 06/Task1/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a class Dog
+Add properties: Name, breed, color
+The dog needs to have an Eat method that returns a message: The dog is now eating.
+A Play method returning a message : The dog is now playing.
+And a ChaseTail method that returns a message: The dog is now chasing it's tail.
+The user needs to create a dog object by using inputs from the console
+            and then choose an option to choose one of the methods mentioned above.
+For example, if he enters 1 to call the Eat method, if he enters 2
+            to call the Play method or if the user enters 3 to call the ChaseTail method.*/
+
+            Console.WriteLine("Write dog's name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Write dog's breed");
+            string breed = Console.ReadLine();
+            Console.WriteLine("Write dog's color");
+            string color = Console.ReadLine();
+            Console.WriteLine("Enter 1,2 or 3");
+            string number = Console.ReadLine();
+
+
+            Dog dog1 = new Dog(name, breed, color);
+            Console.WriteLine($"Your dog's name is {name}, breed - {breed} and it's color is {color}");
+            if (number == "1")
+            {
+                Console.WriteLine($"{dog1.Eat()}");
+            }
+            else if (number == "2")
+            {
+                Console.WriteLine($"{dog1.Play()}");
+            }
+            else if (number == "3")
+            {
+                Console.WriteLine($"{dog1.ChaseTail()}");
+            }
+            else Console.WriteLine($"You entered wrong number");
+
+        }
+    }
+}
