2be3505d47cd6c1c5b0c0b44cca479a7c7c759f3
unknown <elena_mitevska5@yahoo.com>
2024-03-16 10:09:06 +0100

    l

---
+++ Homework 07/Homework07/Homework07/Number.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Homework07
+{
+    public class NumberStats
+    { 
+        public double Number { get; set; }
+
+        public string NumberPosOrNegative()
+        {
+            if (Number < 0) 
+            {
+                return $"The number is negative.";
+            }
+            else if (Number > 0)
+            {
+                return $"The number is positive.";
+            }
+            else return $"Your number is 0.";
+        }
+
+        public string NumberOddOrEven()
+        {
+            if (Number % 2 == 0) 
+            {
+                return $"Your number is even.";
+            }
+            else return $"Your number is odd.";
+        }
+
+        public string DecimalOrInteger()
+        {
+            if (Number % 1 != 0)
+            {
+                return $"Your number is decimal.";
+            }
+            else return $"Your number is integer.";
+        }
+        public NumberStats(double number)
+        {
+            Number = number;
+        }
+        
+        public NumberStats() { }
+
+    }
+}
---
+++ Homework 07/Homework07/Homework07/Program.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Homework07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a method called NumberStats that accepts a number. This method should:
+
+ Find out if the number is negative or positive
+
+ Find out if the number is odd or even
+
+ Find out if the number is decimal or integer
+
+ After finding all the stats it should print them like this:
+
+ Stats for number: 25
+
+ Positive
+ Integer
+ Odd
+ The number should be entered in the console by the user.
+
+ Bonus: Validate if the user is entering a number
+
+ Bonus: Ask the user to press Y to try again or X to exit*/
+            //i/*nt number1;*/
+            double number1;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+                if  (!double.TryParse(input, out number1))
+                {
+        
+               
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+
+               
+                
+                
+                NumberStats number = new NumberStats(number1);
+            Console.WriteLine($"{number.NumberPosOrNegative()}{number.NumberOddOrEven()}{number.DecimalOrInteger()}");
+               
+
+                Console.WriteLine("Do you want to enter another number y/n?");
+                string input1 = Console.ReadLine();
+                if (input1 != "y" && input1 != "Y")
+                {
+                    break;
+                }
+            }
+
+        }
+
+        }
+    }
+
